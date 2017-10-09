a529d2ec9b3226e083b21e4d62789369f3524a21
Matthew Birch <matthew678532@gmail.com>
2017-10-09 18:26:30 +0100

    Began defined build tasks within the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,53 @@ const gulp = require('gulp'),
 			imagemin = require('gulp-imagmin'),
 			rename = require('gulp-rename'),
 			uglify = require('gulp-uglify'),
+			gutil = require('gulp-util'),
 			browserSync = require('browser-sync').create(),
 			pump = require('pump'),
 			del = require('del'),
 			runsequence = require('run-sequence'),
 			cp = require('child_process')
+
+const jekyll = process.platform  === 'win32' ? 'jekyll.bat' : 'jekyll'
+
+gulp.task('jekyll', function(done) {
+	return cp.spawn(jekyll, ['build'], {stdio: 'inherit'})
+		.on('error', err => gutil.log(gutil.colors.red(err.message)))
+		.on('close', done)
+})
+
+gulp.task('sass', function(cb) {
+	pump([
+		gulp.src('src/assets/css/main.sass'),
+		sass(),
+		prefix(),
+		rename({suffix: '.min'}),
+		cssnano(),
+		gulp.dest('dist/assets/css'),
+		browsersync.stream()
+	], cb)
+})
+
+gulp.task('js', function(cb) {
+	pump([
+		gulp.src(['node_modules/jquery/dist/jquery.min.js', 'src/assets/js/modules/*.js', 'src/assets/js/main.js']),
+		concat('main.js'),
+		rename({suffix: '.min'}),
+		uglify(),
+		gulp.dest('dist/assets/js'),
+		browsersync.stream()
+	], cb)
+})
+
+gulp.task('image', function(cb) {
+	pump([
+		gulp.src('src/assets/img/**/*'),
+		cache(imagemin({
+			optimizationLevel: 5,
+			progressive: true,
+			interlaced: true
+		})),
+		gulp.dest('dist/assets/img'),
+		browsersync.stream()
+	], cb)
+})
